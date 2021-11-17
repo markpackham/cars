5eb77a9da1277963e36e6239cc309221068d9531
mark <markpackham1@gmail.com>
2021-11-17 08:47:02 +0000

    create cars table
    https://youtu.be/376vZ1wNYPA?t=13997

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,5 +7,23 @@
                 Cars
             </h1>
         </div>
+        <div class="w-5/6 py-10">
+            <div class="m-auto">
+                <span class="uppercase text-blue-500 font-bold text-xs italic">
+                    Founded: 2020
+                </span>
+                <h2 class="text-gray-700 text-5xl">
+                    Audi
+                </h2>
+                <p class="text-lg text-gray-700 py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo iure
+                    labore necessitatibus consectetur
+                    repudiandae, ducimus a totam minus inventore aliquid ipsa dignissimos earum, id architecto nam numquam.
+                    Dolorum, praesentium?</p>
+                <hr class="mt-4 mb-8">
+            </div>
+        </div>
     </div>
+
+
+
 @endsection
