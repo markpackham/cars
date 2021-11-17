ad049d08d83909714d13cd2d470ee89f19dfd009
mark <markpackham1@gmail.com>
2021-11-17 09:16:36 +0000

    use eloquent to display all cars
    https://youtu.be/376vZ1wNYPA?t=14321

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -8,22 +8,21 @@
             </h1>
         </div>
         <div class="w-5/6 py-10">
-            <div class="m-auto">
-                <span class="uppercase text-blue-500 font-bold text-xs italic">
-                    Founded: 2020
-                </span>
-                <h2 class="text-gray-700 text-5xl">
-                    Audi
-                </h2>
-                <p class="text-lg text-gray-700 py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo iure
-                    labore necessitatibus consectetur
-                    repudiandae, ducimus a totam minus inventore aliquid ipsa dignissimos earum, id architecto nam numquam.
-                    Dolorum, praesentium?</p>
-                <hr class="mt-4 mb-8">
-            </div>
+            @foreach ($cars as $car)
+                <div class="m-auto">
+                    <span class="uppercase text-blue-500 font-bold text-xs italic">
+                        Founded: {{ $car->founded }}
+                    </span>
+                    <h2 class="text-gray-700 text-5xl">
+                        {{ $car->name }}
+                    </h2>
+                    <p class="text-lg text-gray-700 py-6">
+                        {{ $car->description }}
+                    </p>
+                    <hr class="mt-4 mb-8">
+                </div>
+            @endforeach
         </div>
     </div>
-
-
 
 @endsection
